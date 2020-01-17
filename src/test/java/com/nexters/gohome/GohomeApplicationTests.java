79fbde6038a813b9aa141cff67db0e525e0cfbd5
sanghwan kim <sanghwankim@sanghwans-MacBook-Pro.local>
2020-01-17 21:35:07 +0900

    init

---
+++ src/test/java/com/nexters/gohome/GohomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.gohome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GohomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
