a86ab2c26e3fc135d4c1d7263cbc3ee75781ad94
Jelicho <chlwofla0720@naver.com>
2020-01-26 01:13:59 +0900

    add JAP Configuration
    경고 : 개발단계임을 고려하여 cross-origin을 전체로 개방해놓았습니다. 추후 수정이 필요할 것으로 보입니다.

--- src/main/java/com/voa/goodbam/repository/UserRepsitory.java
+++ src/main/java/com/voa/goodbam/repository/UserRepsitory.java
@@ -2,7 +2,9 @@ package com.voa.goodbam.repository;
 
 import com.voa.goodbam.domain.room.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepsitory extends CrudRepository<User, Long> {
 
 }
