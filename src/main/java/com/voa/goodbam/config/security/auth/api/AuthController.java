13510021b21fc75bf76a744dbb69ade2ddfd320e
Jelicho <chlwofla0720@naver.com>
2020-02-06 23:13:15 +0900

    #7 add JWT token generate & AOP auth & kakaoAuth

---
+++ src/main/java/com/voa/goodbam/config/security/auth/api/AuthController.java
@@ -0,0 +1,27 @@
+package com.voa.goodbam.config.security.auth.api;
+
+import com.voa.goodbam.config.security.jwt.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController
+{
+    private final JwtService jwtService;
+
+    public AuthController(JwtService jwtService)
+    {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("refresh")
+    public ResponseEntity renewJwtToken(@RequestHeader("Authorization") String refreshToken)
+    {
+        return new ResponseEntity(jwtService.renewJwtToken(refreshToken),HttpStatus.OK);
+    }
+}
