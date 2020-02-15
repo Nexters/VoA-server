89cef8c529560f488af5ffa71c226684ea1e2f38
sanghwan kim <sanghwankim@sanghwans-MacBook-Pro.local>
2020-02-15 18:51:43 +0900

    ios push done

--- src/main/java/com/voa/goodbam/support/pushnotification/IOSPush.java
+++ src/main/java/com/voa/goodbam/support/pushnotification/IOSPush.java
@@ -5,46 +5,57 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 
+@Slf4j
+@Component
 public class IOSPush {
 
     @PostConstruct
     private void initializeIOS() {
+        log.info("Init firebase");
         try {
             FileInputStream serviceAccount =
-                    new FileInputStream("path/to/serviceAccountKey.json");
+                    new FileInputStream("/voa/voaservice-cf8bf-firebase-adminsdk-xffaz-e2511636b1.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://voaservice-cf8bf.firebaseio.com")
                     .build();
-            FirebaseApp.initializeApp(options);
 
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase application has been initialized");
+            }
         } catch (Exception e) {
-
+            log.error(e.getMessage());
         }
 
     }
 
+
+    @Scheduled(fixedDelay = 10000)
     public void sendNotification() {
-        //https://firebase.google.com/docs/cloud-messaging/send-message
-        String registrationToken = "AAAA697aE94:APA91bHHaB9fGfpKjZTjbxp8EsSgmmX_oS_pEgHePOdMzMC8UGriQvVA6tinmq8yWicCXPgaEYSHW2ELOwFrtR-ggB0OSgwyN0Heve-_DZ83_fMKCskEoU3PoNEJw676HfkPpXHpgZbi";
+        String registrationToken = "dPcYNcKyW09mmTfmeuQD9p:APA91bGp5icdr4i17RT8qX8Fp-9tQnMSTqAKMG7_MBodvMaTOnkRnAW7dVOWXQDpqdfncrvuGanwXvFR_jvA1AYGxymu7Lbne3fKFg2w3GoOmoL1DzMFQQEhO5lpk0XbXJtozIsAFRu7";
 
-            Message message = Message.builder()
-                    .putData("score", "850")
-                    .setToken(registrationToken)
-                    .build();
+        Message message = Message.builder()
+                .putData("HELLLOOO", "HIHIHIHIHHI")
+                .setToken(registrationToken)
+                .setNotification(Notification.builder().setBody("body").setTitle("title").build())
+                .build();
 
-            try {
-                String response = FirebaseMessaging.getInstance().send(message);
-                System.out.println("Successfully sent message: " + response);
-
-            } catch (Exception e) {
-
-            }
+        try {
+            String response = FirebaseMessaging.getInstance().send(message);
+            log.info("Successfully sent message: " + response);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 
 }
