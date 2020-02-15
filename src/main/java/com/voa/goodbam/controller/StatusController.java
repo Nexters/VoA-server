2d4578c2d8ca5f6e8d37b6898402d582b01a1523
sanghwan kim <sanghwankim@sanghwans-MacBook-Pro.local>
2020-02-15 17:35:19 +0900

    update time

--- src/main/java/com/voa/goodbam/controller/StatusController.java
+++ src/main/java/com/voa/goodbam/controller/StatusController.java
@@ -5,13 +5,13 @@ import com.voa.goodbam.domain.roomStatus.InvitationStatus;
 import com.voa.goodbam.domain.roomStatus.UserStatusInRoom;
 import com.voa.goodbam.domain.scheduler.GoodBamScheduler;
 import com.voa.goodbam.repository.UserStatusInRoomRepository;
+import com.voa.goodbam.support.ScheduleTaskService;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/status")
@@ -40,8 +40,8 @@ public class StatusController {
 
     @PutMapping("/invitation")
     public UserStatusInRoom updateInvitationStatus(@RequestParam InvitationStatus invitationStatus,
-                                       @RequestParam long userId,
-                                       @RequestParam long roomId) {
+                                                   @RequestParam long userId,
+                                                   @RequestParam long roomId) {
         UserStatusInRoom userStatusInRoom = userStatusInRoomRepository.findByUserIdAndRoomId(userId, roomId);
         userStatusInRoom.setInvitationStatus(invitationStatus);
         return userStatusInRoomRepository.save(userStatusInRoom);
@@ -49,14 +49,12 @@ public class StatusController {
 
     @PutMapping("/time/remain")
     public UserStatusInRoom updateRemainingTime(@RequestParam int timeRemaining,
-                                                   @RequestParam long userId,
-                                                   @RequestParam long roomId) {
+                                                @RequestParam long userId,
+                                                @RequestParam long roomId) {
         UserStatusInRoom userStatusInRoom = userStatusInRoomRepository.findByUserIdAndRoomId(userId, roomId);
-//        userStatusInRoom.
-
-        if (time.isPresent()) {
-            GoodBamScheduler.schedule(homeComingStatus, userId, time.get());
-        }
+        LocalDateTime expectedArrivalTime = LocalDateTime.now().plusMinutes(timeRemaining);
+        userStatusInRoom.setExpectedToArriveAt(expectedArrivalTime);
+        new ScheduleTaskService().schedule(userStatusInRoom.getTask(), expectedArrivalTime);
         return userStatusInRoomRepository.save(userStatusInRoom);
     }
 }
