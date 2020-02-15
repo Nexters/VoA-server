f584a67bee078d844b27f01dc366de5d9f105e1e
jaerim <chlwofla0720@naver.com>
2020-02-15 23:06:25 +0900

    bug fix

--- src/main/java/com/voa/goodbam/controller/MessengerController.java
+++ src/main/java/com/voa/goodbam/controller/MessengerController.java
@@ -7,10 +7,6 @@ import com.voa.goodbam.domain.messenger.Messenger;
 import com.voa.goodbam.repository.MessengerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
