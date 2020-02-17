5a7079955cd7fd32f3b961d18e6e4ce8369325ef
jaerim <chlwofla0720@naver.com>
2020-02-17 20:45:20 +0900

    bug fix

--- src/main/java/com/voa/goodbam/repository/UserRepository.java
+++ src/main/java/com/voa/goodbam/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByKakaoId(String kakaoId);
+
+    Optional<User> findUserById(Long id);
 }
